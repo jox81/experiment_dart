46cb1bc3e54ee972e16c540ed9c978044d847269
Jérôme Puttemans <jerome.puttemans@gmail.com>
2016-07-28 23:15:28 +0200

    Adding PBR Gamma/Linear

--- webgl/lib/src/materials.dart
+++ webgl/lib/src/materials.dart
@@ -418,6 +418,29 @@ class MaterialPBR extends MaterialCustom {
       return max(0.0, dot(lightDirection, surfaceNormal));
     }
 
+    const float gamma = 2.2;
+
+    //gamma in
+    vec3 toLinear(vec3 v);
+    vec3 toLinear(vec3 v) {
+      return pow(v, vec3(gamma));
+    }
+    vec4 toLinear(vec4 v);
+    vec4 toLinear(vec4 v) {
+      return vec4(toLinear(v.rgb), v.a);
+    }
+
+    // gamma out
+    vec3 toGamma(vec3 v);
+    vec3 toGamma(vec3 v) {
+      return pow(v, vec3(1.0 / gamma));
+    }
+
+    vec4 toGamma(vec4 v);
+    vec4 toGamma(vec4 v) {
+      return vec4(toGamma(v.rgb), v.a);
+    }
+
     void main() {
       //normalize the normal, we do it here instead of vertex
       //shader for smoother gradients
@@ -430,11 +453,11 @@ class MaterialPBR extends MaterialCustom {
       float Id = lambert(L, N);
 
       //surface and light color, full white
-      vec4 baseColor = vec4(1.0);
-      vec4 lightColor = vec4(1.0);
+      vec4 baseColor = toLinear(vec4(1.0));
+      vec4 lightColor = toLinear(vec4(1.0));
 
       vec4 finalColor = vec4(baseColor.rgb * lightColor.rgb * Id, 1.0);
-      gl_FragColor = finalColor;
+      gl_FragColor = toGamma(finalColor);
     }
 
     """;
