dfcb4882d2bb583cb9256e8ad5f4254d5e48d617
jox81 <jerome.puttemans@gmail.com>
2016-09-04 14:55:37 +0200

    switch button UI base

--- webgl/web/application/scenes/main.dart
+++ webgl/web/application/scenes/main.dart
@@ -8,21 +8,53 @@ import 'scene_views/scene_view_pbr.dart';
 import 'scene_views/scene_view_performance_test.dart';
 import 'scene_views/scene_view_primitives.dart';
 import 'scene_views/scene_view_experiment/scene_view_experiment.dart';
+import 'dart:async';
+
+Application application;
 
 main() async {
-  CanvasElement canvas = querySelector('#glCanvas');
-  Application application = new Application(canvas);
 
-  Scene scene;
+  DivElement divTools = new DivElement();
+  DivElement divDebugInfos = new DivElement()..id = 'debugInfos';
+  divTools.children.add(divDebugInfos);
+  ParagraphElement pFps = new ParagraphElement()..id = 'fps';
+  divDebugInfos.children.add(pFps);
+  ParagraphElement pDebugInfosText = new ParagraphElement()..id = 'debugInfosText';
+  divDebugInfos.children.add(pDebugInfosText);
+  document.body.children.add(divTools);
 
-//  scene = new SceneViewBase(application.viewAspectRatio);
-//  scene = new SceneViewFrameBuffer(application.viewAspectRatio);
-//  scene = new SceneViewParticle(application.viewAspectRatio);
-//  scene = new SceneViewPBR(application.viewAspectRatio);
-//  scene = new SceneViewPerformanceTest(application.viewAspectRatio);
-//  scene = new SceneViewPrimitives(application.viewAspectRatio);
-  scene = new SceneViewExperiment(application.viewAspectRatio);
+  ButtonElement buttonSwitch = new ButtonElement()..text = 'swicth scene';
+  divTools.children.add(buttonSwitch);
 
+  DivElement divCanvas = new DivElement();
+  CanvasElement canvas = new CanvasElement()..id = 'glCanvas';
+  divCanvas.children.add(canvas);
+  document.body.children.add(divCanvas);
+
+  application = new Application(canvas);
+
+  List<Scene> scenes = [
+    new SceneViewBase(application.viewAspectRatio),
+    new SceneViewPrimitives(application.viewAspectRatio),
+    new SceneViewPBR(application.viewAspectRatio),
+    new SceneViewFrameBuffer(application.viewAspectRatio),
+    new SceneViewParticle(application.viewAspectRatio),
+    new SceneViewPerformanceTest(application.viewAspectRatio),
+    new SceneViewExperiment(application.viewAspectRatio),
+  ];
+
+  int sceneId = 0;
+
+  buttonSwitch.onClick.listen((e){
+    sceneId++;
+    sceneId %= scenes.length;
+    switchScene(scenes[sceneId]);
+  });
+
+  switchScene(scenes[sceneId]);
+}
+
+Future switchScene (Scene scene)async {
   await scene.setupScene();
   application.render(scene);
 }
