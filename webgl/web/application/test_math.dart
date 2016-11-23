c0ba9ef4fe3fbaf215c624e0b72c296b25f4f358
jox81 <jerome.puttemans@gmail.com>
2016-11-23 22:29:49 +0100

    test page camera matrix

---
+++ webgl/web/application/test_math.dart
@@ -0,0 +1,58 @@
+import 'dart:html';
+import 'dart:web_gl';
+import 'package:vector_math/vector_math.dart';
+import 'dart:typed_data';
+import 'package:gl_enums/gl_enums.dart' as GL;
+import 'package:webgl/src/camera.dart';
+import 'package:webgl/src/materials.dart';
+import 'package:webgl/src/mesh.dart';
+
+void main() {
+  num viewportX = 0.0;
+  num viewportY = 0.0;
+
+  num viewportWidth = 100.0;
+  num viewportHeight = viewportWidth;
+  num aspectRatio = viewportWidth / viewportHeight;
+
+  num pickX = 0.0;
+  num pickY = 0.0;
+  num pickZ = 0.0;
+
+  num left = -viewportWidth * 0.5;
+  num right = viewportWidth * 0.5;
+
+  num bottom = -viewportWidth * 0.5;
+  num top = viewportWidth * 0.5;
+
+  num zNear = viewportWidth * 0.5;
+  num zFar = -viewportWidth * 0.5;
+
+  Vector3 upDirection = new Vector3(0.0, 1.0, 0.0);
+  Vector3 cameraPosition = new Vector3(0.0, 0.0, viewportWidth * 0.5);
+  Vector3 cameraFocusPosition = new Vector3(0.0, 0.0, 0.0);
+  num fovYRadians = radians(45.0);
+
+  Vector3 pickWorld = new Vector3.zero(); //Screen to world found point
+
+  Matrix4 orthoMatrix =
+      makeOrthographicMatrix(left, right, bottom, top, zNear, zFar);
+
+  Matrix4 viewMatrix =
+      makeViewMatrix(cameraPosition, cameraFocusPosition, upDirection);
+
+  Matrix4 perspectiveMatrix =
+      makePerspectiveMatrix(fovYRadians, aspectRatio, zNear, zFar);
+
+  Matrix4 frustrumMatrix =
+      makeFrustumMatrix(left, right, bottom, top, zNear, zFar);
+
+  Matrix4 cameraMatrix = frustrumMatrix;
+
+  for (num i = 0.0; i < 1.0; i += 0.1) {
+    pickX = i * viewportWidth;
+    bool unprojected = unproject(cameraMatrix, viewportX, viewportWidth,
+        viewportY, viewportHeight, pickX, pickY, pickZ, pickWorld);
+    print('$i : $pickWorld');
+  }
+}
