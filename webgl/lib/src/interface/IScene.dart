e0fbc66e8ef0fb1846fe035b5c9b3492d4a20108
Jérôme Puttemans <jerome.puttemans@gmail.com>
2016-12-02 09:00:05 +0100

    adding method recognition

--- webgl/lib/src/interface/IScene.dart
+++ webgl/lib/src/interface/IScene.dart
@@ -37,6 +37,7 @@ abstract class IEditElement{
 
     Map<String, MethodMirror> instance_mirror_getters = new Map();
     Map<String, MethodMirror> instance_mirror_setters = new Map();
+    Map<String, MethodMirror> instance_mirror_functions = new Map();
 
     for (var v in class_mirror.instanceMembers.values) {
       String name = MirrorSystem.getName(v.simpleName);
@@ -46,14 +47,21 @@ abstract class IEditElement{
         instance_mirror_getters[name] = v;
       } else if (v is MethodMirror && v.isSetter && !v.isPrivate ) {
         instance_mirror_setters[name] = v;
+      } else if (v is MethodMirror && v.isRegularMethod) {
+        instance_mirror_functions[name] = v;
       }
     }
 
-    instance_mirror_getters.forEach((String key, MethodMirror getter){
+    instance_mirror_getters.forEach((String key, MethodMirror instance_mirror_field){
       if(instance_mirror_setters.containsKey('$key=')){
-        Symbol fieldSymbol = getter.simpleName;
+        Symbol fieldSymbol = instance_mirror_field.simpleName;
         propertiesInfos[key] = new EditableProperty(instance_mirror.getField(fieldSymbol).reflectee.runtimeType,() => instance_mirror.getField(fieldSymbol).reflectee, (v) => instance_mirror.setField(fieldSymbol, v).reflectee);
       }
+    });
+
+    instance_mirror_functions.forEach((String key, MethodMirror instance_mirror_field) {
+      Symbol fieldSymbol = instance_mirror_field.simpleName;
+      propertiesInfos[key] = new EditableProperty(Function,() => instance_mirror.getField(fieldSymbol).reflectee, null);
     });
 
     return propertiesInfos;
